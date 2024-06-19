21d4327deeef0049d720a5ab3f8a1f3472d5de17
sedat0101 <sfsi_0101@hotmail.com>
2024-06-20 02:27:07 +0300

    template engine

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express')
+const ejs = require('ejs')
 const path = require('path');
 
 const app = express()
+
+// Template Engine
+app.set("view engine", "ejs")
+// Set önceden tanımlı konfigürasyın değişkenleri kullanılır
+// Burada express'e diyoruz ki template engine olarak biz ejs kullancağız
+// ejs bizim klasör yapısında ki views klasörünün içine bakar
+
+
+
 
 //MIDDLEWARES
 app.use(express.static('public'))
 
 
+// Routes 
 app.get('/', (req, res) => {
-     res.sendFile(path.resolve('temp','index.html'))
+    //res.sendFile(path.resolve('temp','index.html'))
+    res.render("index")
 })
+
+app.get('/about', (req, res) => {
+    res.render("about")
+})
+
+app.get('/addPhoto', (req, res) => {
+    res.render("addPhoto")
+})
+
 
 // GET res genel olarak verileri listelemek için kullanılır
 // POST veri göndermek için kullanılır
